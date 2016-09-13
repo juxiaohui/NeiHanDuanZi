f63e719a2d84fd385e40f9c308c61e2b39de794a
juxiaohui <123456>
2016-09-13 10:12:22 +0800

    init

---
+++ NeiHanDuanZi/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  NeiHanDuanZi
+//
+//  Created by juxiaohui on 16/9/13.
+//  Copyright © 2016年 juxiaohui. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ NeiHanDuanZi/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  NeiHanDuanZi
+//
+//  Created by juxiaohui on 16/9/13.
+//  Copyright © 2016年 juxiaohui. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
